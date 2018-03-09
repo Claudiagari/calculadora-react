6687ebf5d9484e8fdbf5fcdd1bc689eff44ab500
valeriavalles <valeria.sv@outlook.es>
2018-03-09 17:37:12 -0500

    calculadora

--- src/componets/Uno.js
+++ src/componets/Uno.js
@@ -3,7 +3,39 @@ class Uno extends Component {
   render() {
     return (
       <div className="uno">
-       <h1>Hola</h1>
+        <div className="resul">
+        </div>
+        <div className="Numbers">
+          <div>
+            <div>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            </div> 
+           <div> 
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+           </div>
+           <div> 
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            </div> 
+        </div>
+        <div className="Oper">
+           
+            <button>+</button>
+            <button>-</button>
+            <button>x</button>
+           
+        </div>     
+        </div> 
+        <div className="ContentList">
+          <div className="Conten-list">
+          </div>
+        </div> 
+
       </div>
     );
   }
