a70dbe9cba297edba5bffc8f2e9c1a58faf8a83a
carlacentenor <carlacentenor@gmail.com>
2018-03-09 22:01:15 -0500

    funcionalidad operaciones

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
-import Uno from './componets/Uno';
-import store from './store/todo'
-
+import Calculator from './Calculator';
+import store from './store/';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <Uno store={store}/>
-     
+        <Calculator store={store} />
       </div>
     );
   }
